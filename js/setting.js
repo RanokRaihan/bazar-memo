b9180f7c6372e57f8718092f5b2e1fdf033b8240
Ranok Raihan <ranokraihan@gmail.com>
2022-07-30 23:41:55 +0600

    setting fixed

--- js/setting.js
+++ js/setting.js
@@ -1,68 +1,72 @@
 renderAllItem(); //helper.js
 const alignButton = document.querySelectorAll(".align-button");
 const buttonSelector = () => {
-    const currentButton = localStorage.getItem("align");
-    if (currentButton === "left") {
-        $(".left-button").classList.add("active");
-        $(".right-button").classList.remove("active");
-    } else {
-        $(".left-button").classList.remove("active");
-        $(".right-button").classList.add("active");
-    }
+  const currentButton = localStorage.getItem("align");
+  if (currentButton === "left") {
+    $(".left-button").classList.add("active");
+    $(".right-button").classList.remove("active");
+  } else {
+    $(".left-button").classList.remove("active");
+    $(".right-button").classList.add("active");
+  }
 };
 
 buttonSelector();
 alignButton.forEach((button) => {
-    button.addEventListener("click", (e) => {
-        localStorage.setItem("align", e.target.dataset.align);
-        buttonSelector();
-    });
+  button.addEventListener("click", (e) => {
+    localStorage.setItem("align", e.target.dataset.align);
+    buttonSelector();
+  });
 });
 
 const deleteItem = (event) => {
-    event.stopPropagation();
-    const oldItems = fetchLocal("all-item");
-    const index = oldItems.indexOf(event.target.dataset.itemname);
-    oldItems.splice(index, 1);
+  event.stopPropagation();
+  const oldItems = fetchLocal("all-item");
+  const index = oldItems.indexOf(event.target.dataset.itemname);
+  oldItems.splice(index, 1);
 
-    setLocal("all-item", oldItems);
-    renderAllItem();
+  setLocal("all-item", oldItems);
+  renderAllItem();
 };
 
 $(".reset-list").addEventListener("click", () => {
-    const items = [
-        "আলু",
-        "মরিচ",
-        "পেয়াজ",
-        "রসুন",
-        "আদা",
-        "ফুলকপি",
-        "পাতাকপি",
-        "শিম",
-        "টমেটো",
-        "বেগুন",
-        "লালশাক",
-        "পালংশাক",
-        "মিষ্টি কুমড়া",
-        "লাউ",
-        "কলা",
-        "লেবু",
-        "শশা",
-        "মাছ",
-        "মুরগি",
-        "গরুর মাংস",
-    ];
-    setLocal("all-item", items);
-    renderAllItem();
+  const items = [
+    "আলু",
+    "মরিচ",
+    "পেয়াজ",
+    "রসুন",
+    "আদা",
+    "ফুলকপি",
+    "পাতাকপি",
+    "শিম",
+    "টমেটো",
+    "বেগুন",
+    "লালশাক",
+    "পালংশাক",
+    "মিষ্টি কুমড়া",
+    "লাউ",
+    "কলা",
+    "লেবু",
+    "শশা",
+    "মাছ",
+    "মুরগি",
+    "গরুর মাংস",
+  ];
+  setLocal("all-item", items);
+  renderAllItem();
 });
 $(".clear-list").addEventListener("click", () => {
-    setLocal("all-item", []);
-    renderAllItem();
+  setLocal("all-item", []);
+  renderAllItem();
 });
 
 $(".add-new-button").addEventListener("click", () => {
-    const newItem = $(".add-input").value;
-    updateLocal("all-item", newItem);
-    $(".add-input").value = "";
-    renderAllItem();
+  const newItem = $(".add-input").value;
+  updateLocal("all-item", newItem);
+  $(".add-input").value = "";
+  renderAllItem();
+});
+
+$(".home").addEventListener("click", () => {
+  window.history.back();
 });
